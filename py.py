869c575de463c4fdba9a1ecf916ee50e95dcde68
PythonBot0 <90237430+PythonBot0@users.noreply.github.com>
2021-09-16 16:22:30 +1000

    Add files via upload

---
+++ py.py
@@ -0,0 +1,11 @@
+print('''iuouiuiuiuouiuoiouiuouiooiuuiuoiuouioiuoiuooiuouiuioou
+iiouuoiuoiuioiouiouiuioiuoiuiuiuioiouiuouioiouiouuiooiui
+ouoipuoiuoiuoiuoiuoiuoiuoiuiouoiuoiuoiuoiuoiuoiuoiuoiuioi
+uoiuoiuoiuoiuoiuoiuoiuoiuoiuoiuoiuoiuoiuoiuiuoiuoi
+uoiuoiuoiuoiuoiuoiuoiuoiuoiuoiuoiuoiuiiuoiouiuoiuoiu
+oiuoioiuoiuiouiouoiuoiuiiuuoiuoiuiou''')
+m5 = 'm5'
+i5555 = [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]
+for eee in i5555:
+    print(5)
+    print(m5)
